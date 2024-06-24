940b9b8e89fceb70de4b3ea112f48a6e6edadc9e
Soroosh Sanatkhani <sorooshsanatkhani@outlook.com>
2024-06-24 13:19:40 -0400

    update

--- FUS_Toolbox_Arduino/FUS_Toolbox_Arduino.ino
+++ FUS_Toolbox_Arduino/FUS_Toolbox_Arduino.ino
@@ -45,6 +45,7 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     String receivedData = Serial.readStringUntil('\n');
+    Serial.println("ACK");
     if (receivedData.charAt(0) == 'O') { // Check if the received data is the ON command
       digitalWrite(ENA_LR_Pin, LOW);
       digitalWrite(ENA_UD_Pin, LOW);
@@ -60,48 +61,51 @@ void loop() {
     }
     else {
       stopRequested = false; // Reset the stop flag
-      char direction = receivedData.charAt(0);
-      int firstCommaIndex = receivedData.indexOf(',');
-      int secondCommaIndex = receivedData.indexOf(',', firstCommaIndex + 1);
-      float distance = receivedData.substring(firstCommaIndex + 1, secondCommaIndex).toFloat();
-      float speed = receivedData.substring(secondCommaIndex + 1).toFloat();
-      float TravelTime = distance/speed;
-      // Calculate steps and delay
-      long steps = 20 * distance * microsteps;
-      long Frequency = steps/TravelTime;
-      long HalfPeriod = double((1000000.0)/(2.0*Frequency)); // Delay in microseconds
-      Serial.print("Direction: ");
-      Serial.print(direction);
-      Serial.print(",  Distance: ");
-      Serial.print(distance);
-      Serial.print("mm,  Speed: ");
-      Serial.print(speed);
-      Serial.print("mm/s,  Steps: ");
-      Serial.print(steps);
-      Serial.print("  ,  Frequency: ");
-      Serial.print(Frequency/1000.0);
-      Serial.print("kHz  ,  Half Period: ");
-      Serial.print(HalfPeriod);
-      Serial.println(" microseconds");
-      // Control Left-Right movement
-      if (direction == 'L' || direction == 'R') {
-        controlStepper(DIR_LR_Pin, PUL_LR_Pin, ENA_LR_Pin, direction == 'L', steps, HalfPeriod);
-      }
-      // Control Up-Down movement
-      else if (direction == 'U' || direction == 'D') {
-        controlStepper(DIR_UD_Pin, PUL_UD_Pin, ENA_UD_Pin, direction == 'U', steps, HalfPeriod);
-      }
-      // Control Forward-Backward movement
-      else if (direction == 'F' || direction == 'B') {
-        controlStepper(DIR_FB_Pin, PUL_FB_Pin, ENA_FB_Pin, direction == 'F', steps, HalfPeriod);
-      }
-    }
+      processMovementCommand(receivedData);
   }
   if (stopRequested) {
     stopMotors();
   }
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
+void processMovementCommand(String command) {
+  char direction = command.charAt(0);
+  int firstCommaIndex = command.indexOf(',');
+  int secondCommaIndex = command.indexOf(',', firstCommaIndex + 1);
+  float distance = command.substring(firstCommaIndex + 1, secondCommaIndex).toFloat();
+  float speed = command.substring(secondCommaIndex + 1).toFloat();
+  float TravelTime = distance/speed;
+  // Calculate steps and delay
+  long steps = 20 * distance * microsteps;
+  long Frequency = steps/TravelTime;
+  long HalfPeriod = double((1000000.0)/(2.0*Frequency)); // Delay in microseconds
+  Serial.print("Direction: ");
+  Serial.print(direction);
+  Serial.print(",  Distance: ");
+  Serial.print(distance);
+  Serial.print("mm,  Speed: ");
+  Serial.print(speed);
+  Serial.print("mm/s,  Steps: ");
+  Serial.print(steps);
+  Serial.print("  ,  Frequency: ");
+  Serial.print(Frequency/1000.0);
+  Serial.print("kHz  ,  Half Period: ");
+  Serial.print(HalfPeriod);
+  Serial.println(" microseconds");
+  // Control Left-Right movement
+  if (direction == 'L' || direction == 'R') {
+    controlStepper(DIR_LR_Pin, PUL_LR_Pin, ENA_LR_Pin, direction == 'L', steps, HalfPeriod);
+  }
+  // Control Up-Down movement
+  else if (direction == 'U' || direction == 'D') {
+    controlStepper(DIR_UD_Pin, PUL_UD_Pin, ENA_UD_Pin, direction == 'U', steps, HalfPeriod);
+  }
+  // Control Forward-Backward movement
+  else if (direction == 'F' || direction == 'B') {
+    controlStepper(DIR_FB_Pin, PUL_FB_Pin, ENA_FB_Pin, direction == 'F', steps, HalfPeriod);
+  }
+}
+
 void controlStepper(byte dirPin, byte pulPin, byte enaPin, bool direction, long steps, long HalfPeriod) {
   digitalWrite(dirPin, direction ? HIGH : LOW); // Set direction
   delay(300); //minimum enable time is 200ms
@@ -125,4 +129,6 @@ void stopMotors() {
   digitalWrite(PUL_LR_Pin, LOW);
   digitalWrite(PUL_UD_Pin, LOW);
   digitalWrite(PUL_FB_Pin, LOW);
+
+  stopRequested = false;
 }
