f3e09a59552c55d580d4ac07612da8f8e7133cbb
Soroosh Sanatkhani <sorooshsanatkhani@outlook.com>
2024-06-24 02:01:12 -0400

    update

--- Move.cpp
+++ Move.cpp
@@ -16,25 +16,32 @@ void Gantry::Move(char Direction, float Distance, float Speed)
 	{
 		case 'R':
 			gantryPosition.x += Distance;
+			fus_mainwindow->emitPrintSignal("Going right");
 			//break;
 		case 'L':
 			gantryPosition.x -= Distance;
+			fus_mainwindow->emitPrintSignal("Going left");
 			//break;
 		case 'U':
 			gantryPosition.z += Distance;
+			fus_mainwindow->emitPrintSignal("Going up");
 			//break;
 		case 'D':
 			gantryPosition.z -= Distance;
+			fus_mainwindow->emitPrintSignal("Going down");
 			//break;
 		case 'F':
 			gantryPosition.y += Distance;
+			fus_mainwindow->emitPrintSignal("Going forward");
 			//break;
 		case 'B':
 			gantryPosition.y -= Distance;
+			fus_mainwindow->emitPrintSignal("Going backward");
 			//break;
 	}
 	arduino->write(Direction, Distance, Speed);
 	fus_mainwindow->ui.Gantry_x_spinBox->setValue(gantryPosition.x);
 	fus_mainwindow->ui.Gantry_y_spinBox->setValue(gantryPosition.y);
 	fus_mainwindow->ui.Gantry_z_spinBox->setValue(gantryPosition.z);
+	fus_mainwindow->emitPrintSignal("position updated");
 }
